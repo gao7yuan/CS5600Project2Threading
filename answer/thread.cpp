a375923b410bdc981ef6cd6325068d4730c6c8a5
gao7yuan <gao7yuan@northeastern.edu>
2020-02-03 16:59:00 -0800

    implemented sleep

--- answer/thread.cpp
+++ answer/thread.cpp
@@ -9,6 +9,8 @@
 #include "thread_lock.h"
 
 const char* readyList = NULL;
+const char* sleepingThreadMap = NULL;
+
 
 Thread* createAndSetThreadToRun(const char* name,
                                 void* (*func)(void*),
@@ -57,12 +59,37 @@ Thread* nextThreadToRun(int currentTick) {
 
 void initializeCallback() {
     readyList = createNewList();
+    sleepingThreadMap = CREATE_MAP(Thread*);
+    const char* lockThreadMap = CREATE_MAP(const char*); // lock -> thread
 }
 
 void shutdownCallback() {
     destroyList(readyList);
 }
 
-int tickSleep(int numTicks) {}
+int tickSleep(int numTicks) {
+    int start_tick, end_tick, cur_tick;
+    start_tick = getCurrentTick(); // start tick is the tick when the function is called
+    end_tick = start_tick + numTicks; // end tick is calculated
+
+    // find current thread, remove if from ready list,
+    // stop executing it, and append it to the end of ready list
+    Thread* curThread = getCurrentThread();
+    removeFromList(readyList, (void*) curThread);
+    stopExecutingThreadForCycle();
+    addToListIndex(readyList, listSize(readyList) + 1, (void*) curThread);
+
+    // add <thread, end tick> to sleepingThreadMap
+    int* end_tick_ptr = (int*) malloc(sizeof(int));
+    &end_tick_ptr = end_tick;
+    PUT_IN_MAP(Thread*, sleepingThreadMap, curThread, (void*) end_tick_ptr);
+
+    cur_tick = getCurrentTick(); // keep track of current tick
+    // terminate the while loop if current tick has reached end tick
+    while (cur_tick < end_tick) {
+        cur_tick = getCurrentTick(); // update current tick
+    }
+    return start_tick;
+}
 
 void setMyPriority(int priority) {}
